4df8ca5e05724364fa7b35633d73f858c30a2841
Tho Nguyen Duc <ndtho8205@gmail.com>
2020-05-10 19:25:43 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
-import sys
 from pathlib import Path
 
 from setuptools import setup
 
-assert sys.version_info >= (3, 6, 0), "cptool requires Python 3.6+"
-
 CURRENT_DIR = Path(__file__).parent
 
 DESCRIPTION = "A simple toolkit to test your codes on a Competitive Programming."
-README = (CURRENT_DIR / "README.md").read_text(encoding="utf8")
+README = (CURRENT_DIR / "README.md").read_text(encoding="utf-8")
 VERSION = "0.1.0"
 
 setup(
@@ -16,10 +13,14 @@ setup(
     version=VERSION,
     description=DESCRIPTION,
     long_description=README,
+    long_description_content_type="text/markdown",
     author="Tho Nguyen",
     url="https://github.com/ndtho8205/competitive-programming-toolkit",
     python_requires=">=3.6.0",
-    packages=["cptool"],
+    package_dir={"": "."},
+    include_package_data=True,
+    packages=["cptool", "cptool.*"],
     install_requires=["beautifulsoup4>=4.9.0", "ruamel.yaml>=0.16.10"],
     entry_points={"console_scripts": ["cptool = cptool.cli:main"]},
+    zip_safe=False,
 )
