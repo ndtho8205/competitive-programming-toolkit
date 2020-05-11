8da3edde704cb26e6164846436332a44dca1e4fd
Tho Nguyen Duc <ndtho8205@gmail.com>
2020-05-12 02:59:16 +0900

    Update project description.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 CURRENT_DIR = Path(__file__).parent
 
-DESCRIPTION = "A simple toolkit to test your codes on a Competitive Programming."
+DESCRIPTION = "A simple toolkit for Competitive Programming."
 README = (CURRENT_DIR / "README.md").read_text(encoding="utf-8")
 VERSION = "0.2.0"
 
@@ -17,9 +17,9 @@ setup(
     author="Tho Nguyen",
     url="https://github.com/ndtho8205/competitive-programming-toolkit",
     python_requires=">=3.6.0",
+    packages=find_packages(where="."),
     package_dir={"": "."},
     include_package_data=True,
-    packages=["cptool"],
     install_requires=["beautifulsoup4>=4.9.0", "ruamel.yaml>=0.16.10"],
     entry_points={"console_scripts": ["cptool = cptool:run_main"]},
     zip_safe=False,
